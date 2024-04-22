7e9c81f28ae478d23cc9fa549d1f922fc3b35577
Sky_Interest <841102500@qq.com>
2024-04-22 16:36:40 +0800

    2.定义函数

--- Contacts/contact_book.php
+++ Contacts/contact_book.php
@@ -1,4 +1,13 @@
-<?php
+<?php 
     //1.初始化联系人数组
-    $contact = ["email" => "114@example.com", "phone" => "114514"]
+    $contact = ["xiaoming" => ["email" => "114@example.com", "phone" => "114514"],
+                "xiaohua"=> ["email"=>"514@example.com","phone"=> "191980"]
+            ];
+    //2.定义显示所有联系人的函数
+    function displayContact( $contacts) {
+        foreach( $contacts as $name => $info){
+            echo"Name: $name , Email: ".$info['email']." ,Phone :".$info['phone']."\n";
+        }
+    }
+        
 ?>
