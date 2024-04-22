bbc796cbe9b2658ca2c5d3b962a7e00b3cc520f4
Sky_Interest <841102500@qq.com>
2024-04-22 20:16:39 +0800

    4.引入Include后文件主体

---
+++ Class_evaluate/inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="text-center mt-5">
+        版权所有 &copy; 2024
+    </footer>
+
+    <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ Class_evaluate/inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <title>课程评价</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">课程评价中心</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="切换导航">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.html">首页</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./reviews.html">评价</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.html">关于我们</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
