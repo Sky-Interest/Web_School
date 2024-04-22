816d57b83457e5c80a3a6c9f81e529c6b6e435f5
Sky_Interest <841102500@qq.com>
2024-04-22 16:44:54 +0800

    3.添加联系人

--- Contacts/contact_book_v1.php
+++ Contacts/contact_book_v1.php
@@ -1,6 +1,6 @@
 <?php 
     //1.初始化联系人数组
-    $contact = ["xiaoming" => ["email" => "114@example.com", "phone" => "114514"],
+    $contacts = ["xiaoming" => ["email" => "114@example.com", "phone" => "114514"],
                 "xiaohua"=> ["email"=>"514@example.com","phone"=> "191980"]
             ];
     //2.定义显示所有联系人的函数
@@ -9,5 +9,9 @@
             echo"Name: $name , Email: ".$info['email']." ,Phone :".$info['phone']."\n";
         }
     }
+    //3.添加联系人
+    $contacts['xiaoli'] = ["email" =>"115@example.com", "phone" => "1234321"];
+
+    
         
 ?>
