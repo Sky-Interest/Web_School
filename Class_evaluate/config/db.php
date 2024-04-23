6b6d564c4dba0f08d1c11b37520ee8c0328d4ce0
Sky_Interest <841102500@qq.com>
2024-04-23 09:04:24 +0800

    创建基本数据库连接

---
+++ Class_evaluate/config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','interest');
+define('DB_PASS',123456);
+define('DB_NAME','review_app');
+
+//新建连接
+
+$conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+//检查连接
+if ($conn->connect_error) {
+    die('连接失败！'. $conn->connect_error);
+}
+echo"连接成功！";
+
+?>
