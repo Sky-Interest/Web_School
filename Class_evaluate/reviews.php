2db974fc780664d2549cbc87141c999435118c19
Sky_Interest <841102500@qq.com>
2024-04-24 15:26:06 +0800

    循环遍历评价数据

--- Class_evaluate/reviews.php
+++ Class_evaluate/reviews.php
@@ -1,50 +1,49 @@
-<?php include 'inc/header.php';?>
+<?php include 'inc/header.php'; ?>
 
 
-<?php
-    $reviews = [
-        [
-            'id' => 1,
-            'name'=> '张三',
-            'course_name'=> 'test_course',
-            'context'=> 'test_context',
-            'date'=> 'test_date',
-        ],
-        [
-            'id' => 2,
-            'name'=> '张三',
-            'course_name'=> 'test_course',
-            'context'=> 'test_context',
-            'date'=> 'test_date',
-        ],
-        [
-            'id' => 3,
-            'name'=> '张三',
-            'course_name'=> 'test_course',
-            'context'=> 'test_context',
-            'date'=> 'test_date',
+
+<main>
+    <div class="container d-flex flex-column align-items-center">
+        <?php
+        $reviews = [
+            [
+                'id' => 1,
+                'name' => '张三',
+                'course_name' => 'test_course',
+                'context' => 'test_context',
+                'date' => 'test_date',
+            ],
+            [
+                'id' => 2,
+                'name' => '张三',
+                'course_name' => 'test_course',
+                'context' => 'test_context',
+                'date' => 'test_date',
+            ],
+            [
+                'id' => 3,
+                'name' => '张三',
+                'course_name' => 'test_course',
+                'context' => 'test_context',
+                'date' => 'test_date',
+            ]
         ]
-    ]
-?>
-    <main>
-        <div class="container d-flex flex-column align-items-center">
-            <h2>课程评价</h2>
-            <div class="card my-3" style="width: 18rem;">
-                <div class="card-body">
-                    这是一门非常实用的课程，深入浅出地介绍了编程基础。老师讲解清晰，配套资料丰富，非常推荐给编程新手。
-                </div>
-            </div>
-            <div class="card my-3" style="width: 18rem;">
-                <div class="card-body">
-                    我通过这门课程掌握了数据分析的核心技术，课程内容覆盖广泛，案例实战让我受益匪浅，值得一学。
-                </div>
-            </div>
-            <div class="card my-3" style="width: 18rem;">
-                <div class="card-body">
-                    作为一个设计背景的学生，这门课程帮我打开了前端开发的大门。课程结构合理，由浅入深，让我能够在短时间内构建出自己的网站。
-                </div>
+        ?>
+
+        <?php if (empty($reviews)) : ?>
+            <p class="lead mt3">目前没有课程评价！</p>
+        <?php endif; ?>
+
+        <h2>课程评价</h2>
+        <?php foreach($reviews as $review):?>
+        <div class="card my-3" style="width: 18rem;">
+            <div class="card-body">
+                这是一门非常实用的课程，深入浅出地介绍了编程基础。老师讲解清晰，配套资料丰富，非常推荐给编程新手。
             </div>
         </div>
-    </main>
+        <?php endforeach; ?>
+        
+    </div>
+</main>
 
 <?php include 'inc/footer.php'; ?>
