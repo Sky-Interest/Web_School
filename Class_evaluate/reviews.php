efae010d08152a550c2b9529c63da6441657a498
Sky_Interest <841102500@qq.com>
2024-05-15 14:05:46 +0800

    4.3 添加日期

--- Class_evaluate/reviews.php
+++ Class_evaluate/reviews.php
@@ -16,27 +16,31 @@
         <?php endif; ?>
 
         <h2>课程评价</h2>
-        <?php foreach($reviews as $review):?>
-        <div class="card my-3" style="width: 18rem;">
-            <div class="card-title text-center mt-2">
-                <?php 
-                echo $review['course_name'];
-                ?>
-            </div>
-
-            <div class="card-body text-center">
-                <!-- 引用context数据 -->
-                "
-                <?php echo $review ['content'];?>
-                "
-                <div class="text-secondary mt-2">
-                    来自<?php echo $review['name']; ?>
+        <?php foreach ($reviews as $review) : ?>
+            <div class="card my-3" style="width: 18rem;">
+                <div class="card-title text-center mt-2">
+                    <?php
+                    echo $review['course_name'];
+                    ?>
                 </div>
-                
+
+                <div class="card-body text-center">
+                    <!-- 引用context数据 -->
+                    "
+                    <?php echo $review['content']; ?>
+                    "
+                    <div class="text-secondary mt-2">
+                        来自<?php echo $review['name']; ?>
+                    </div>
+
+                    <div class="text-secondary mt-2">
+                        <?php echo $review['date']; ?>
+                    </div>
+
+                </div>
             </div>
-        </div>
         <?php endforeach; ?>
-        
+
     </div>
 </main>
 
